821644989916171027bd7b3755d588321b7440a7
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2021-12-20 16:01:46 +0530

    repository-interface-created

---
+++ src/main/java/com/haven/muffinserver/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.haven.muffinserver.repository;
+
+import com.haven.muffinserver.model.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByEmail(String email);
+}
