f5056a340c22e05b888eeac7d3a8fc4f2324d5eb
Sai SriHarsha Griddaluru <sriharsha.9554@gmail.com>
2021-12-20 15:51:42 +0530

    spring-init-completed

---
+++ src/main/java/com/haven/muffinserver/MuffinServerApplication.java
@@ -0,0 +1,13 @@
+package com.haven.muffinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuffinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuffinServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haven/muffinserver/MuffinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haven.muffinserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuffinServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
